756413d53b7f52e48f43ab13b4a7b160b52f3bba
Ivan <istarcevic16@gmail.com>
2023-03-18 00:27:26 +0100

    feature: add custom hook for carousel feature

---
+++ src/hooks/useResponsiveCarousel.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+const useResponsiveCarousel = () => {
+  const [slidesToShow, setSlidesToShow] = useState(3);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setSlidesToShow(1);
+      } else {
+        setSlidesToShow(3);
+      }
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return slidesToShow;
+};
+
+export default useResponsiveCarousel;
