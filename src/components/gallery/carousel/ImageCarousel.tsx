717558dcfa65c74499c89b9430debc51f7ea812b
Ivan <istarcevic16@gmail.com>
2023-03-18 16:29:55 +0100

    feature: carousel

--- src/components/gallery/carousel/ImageCarousel.tsx
+++ src/components/gallery/carousel/ImageCarousel.tsx
@@ -1,39 +1,34 @@
 import React from "react";
 import Carousel from "nuka-carousel";
-import { CenterLeftControl, CenterRightControl } from "./components/CarouselControls";
-import useResponsiveCarousel from "../../../hooks/useResponsiveCarousel";
-
-interface CarouselImage {
-  id: number;
-  url: string;
-}
+import { CenterLeftControl, CenterRightControl } from "../index";
+import { useResponsiveCarousel } from "../../../hooks";
+import { Image } from "../../../types";
 
 interface CarouselProps {
-  images: CarouselImage[];
+  images: Image[];
 }
 
 export const ImageCarousel: React.FC<CarouselProps> = ({ images }) => {
   const slidesToShow = useResponsiveCarousel();
 
   return (
-    <>
-      <div className="flex flex-col relative justify-center items-center h-[30em] pb-[2em] 2xl:pb-[10em] ">
-        <Carousel
-          wrapAround
-          slidesToShow={slidesToShow}
-          cellSpacing={10}
-          swiping
-          dragging
-          renderBottomCenterControls={() => null} // remove the dots/pagination
-          renderCenterLeftControls={(props) => <CenterLeftControl {...props} />}
-          renderCenterRightControls={(props) => <CenterRightControl {...props} />}
-        >
-          {images.map((image, index) => (
-            <img key={image.id} src={image.url} alt={`Slide ${index}`} className="w-full h-full object-cover" />
-          ))}
-        </Carousel>
-        <div className="flex justify-between w-full max-w-4xl mt-4 mb-5 "></div>
-      </div>
-    </>
+    <div className="flex flex-col relative justify-center items-center h-[30em] pb-[2em] 2xl:pb-[10em] ">
+      <Carousel
+        wrapAround
+        slidesToShow={slidesToShow}
+        cellSpacing={10}
+        swiping
+        dragging
+        zoomScale={0.9}
+        renderBottomCenterControls={() => null} // remove the dots/pagination
+        renderCenterLeftControls={(props) => <CenterLeftControl {...props} />}
+        renderCenterRightControls={(props) => <CenterRightControl {...props} />}
+      >
+        {images.map((image, index) => (
+          <img key={image.id} src={image.url} alt={`Slide ${index}`} className="w-full h-full object-cover" />
+        ))}
+      </Carousel>
+      <div className="flex justify-between w-full max-w-4xl mt-4 mb-5 "></div>
+    </div>
   );
 };
--- src/components/gallery/carousel/components/CarouselControls.tsx
+++ src/components/gallery/carousel/components/CarouselControls.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { carouselArrow } from "../../../../constants";
+import React, { FC } from "react";
+import { CAROUSEL_ARROW } from "../../../../constants";
 
 interface CarouselControlProps {
   direction: "left" | "right";
   onClick: () => void;
 }
 
-const CarouselControl: React.FC<CarouselControlProps> = ({ direction, onClick }) => {
+const CarouselControl: FC<CarouselControlProps> = ({ direction, onClick }) => {
   const rotation = direction === "left" ? "rotate-180" : "";
 
   return (
     <button onClick={onClick} className="cursor-pointer">
       <img
-        src={carouselArrow}
+        src={CAROUSEL_ARROW}
         alt={`${direction === "left" ? "Previous" : "Next"} Arrow`}
         className={`w-10 h-10 ${rotation} absolute bottom-0 ${direction === "left" ? "left-1/3" : "right-1/3"}`}
       />
@@ -25,10 +25,10 @@ interface CarouselRenderControlProps {
   nextSlide?: () => void;
 }
 
-export const CenterLeftControl: React.FC<CarouselRenderControlProps> = ({ previousSlide }) => (
+export const CenterLeftControl: FC<CarouselRenderControlProps> = ({ previousSlide }) => (
   <CarouselControl direction="left" onClick={previousSlide!} />
 );
 
-export const CenterRightControl: React.FC<CarouselRenderControlProps> = ({ nextSlide }) => (
+export const CenterRightControl: FC<CarouselRenderControlProps> = ({ nextSlide }) => (
   <CarouselControl direction="right" onClick={nextSlide!} />
 );
