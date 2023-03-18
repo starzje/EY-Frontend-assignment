cbb1d9c119b3f798e7a0bca2940d73157fdd5be0
Ivan <istarcevic16@gmail.com>
2023-03-18 16:31:29 +0100

    feature: updated main entry

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
-import * as React from "react";
-import { Gallery, Footer, Article } from "./components";
-import { articleImages, heroImage, catSectionParagraph, dogSectionParagraph } from "./constants";
+import React from "react";
+import { Gallery, Footer, Article, Contact, CookieBanner } from "./components";
+import { ARTICLE_IMAGES, HERO_IMAGE, CAT_PARAGRAPH, DOG_PARAGRAPH } from "./constants";
 
 const App = () => {
   return (
     <>
       <div className="bg-primary-400 text-black pb-16">
-        <img src={heroImage} alt="Header Image" className="w-full" />;
+        <img src={HERO_IMAGE} alt="Header Image" className="w-full" />;
         <div className="container min-h-screen ">
           <h1 className="title">Daily pet facts - Love!</h1>
           <main className="space-y-24 md:space-y-32">
-            <Article img={articleImages.cat} imgAlt="Image of a cat" title="The cat">
-              {catSectionParagraph}
+            <Article img={ARTICLE_IMAGES.cat} imgAlt="Image of a cat" title="The cat">
+              {CAT_PARAGRAPH}
             </Article>
-            <Article img={articleImages.dog} imgAlt="Image of a dog" title="The dog" flexDirection="flex-row-reverse">
-              {dogSectionParagraph}
+            <Article img={ARTICLE_IMAGES.dog} imgAlt="Image of a dog" title="The dog" flexDirection="flex-row-reverse">
+              {DOG_PARAGRAPH}
             </Article>
           </main>
         </div>
       </div>
       <h2 className="title mb-2">Gallery</h2>
       <Gallery />
+      <Contact />
       <Footer />
+      <CookieBanner />
     </>
   );
 };
